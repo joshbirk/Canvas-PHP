1a2ea2d4a85f3e54dc32e55087b693f89d9861e8
joshbirk <joshua.birk@salesforce.com>
2013-01-26 17:37:05 -0600

    /Users/joshua.birk/rungit.sh

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-require('src/Httpful/Bootstrap.php');
+require('httpful/src/Httpful/Bootstrap.php');
 \Httpful\Bootstrap::init();
 
 $consumer_secret = $_ENV['secret'];
